ff0d0011024893a4d1c3f8abb92f5619e017e850
danielmartin0 <github@danielgmartin.com>
2025-02-04 02:36:00 +0000

    see changelog.txt

--- data-updates.lua
+++ data-updates.lua
@@ -21,6 +21,9 @@ end
 if data.raw["space-location"]["solar-system-edge"] then
 	data.raw["space-location"]["solar-system-edge"].redrawn_connections_length_multiplier = 4.8
 end
+if data.raw["space-location"]["solar-system-edge"] then
+	data.raw["space-location"]["shattered-planet"].redrawn_connections_length_multiplier = 100 -- Has no effect in vanilla
+end
 
 -- == Maraxsis ==--
 
