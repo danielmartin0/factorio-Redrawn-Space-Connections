abd8923377c7747eb33ee13313bda3bd976f5a96
danielmartin0 <github@danielgmartin.com>
2025-06-06 02:37:01 +0100

    see changelog.txt

--- settings.lua
+++ settings.lua
@@ -3,8 +3,8 @@ data:extend({
 		type = "bool-setting",
 		setting_type = "startup",
 		name = "Redrawn-Space-Connections-real-space-triangulation",
-		forced_value = false,
-		default_value = false,
+		forced_value = true,
+		default_value = true,
 		hidden = true,
 	},
 	{
